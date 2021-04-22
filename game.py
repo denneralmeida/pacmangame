e2fc66be8caa8b9ce2c2427df54fc89fad9d07f8
Denner Vinicius de Almeida Moreira <denner.moreira@softplan.com.br>
2021-04-22 19:39:34 -0300

    #Adiciona versao 2 do projeto

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 from conf import *
-from pacman import *
+from pacman import Pacman, Maze, pygame
 import os
+
+# pylint: disable=maybe-no-member
 
 
 class Game:
@@ -10,8 +12,7 @@ class Game:
         self.frame = None
         self.blocks = None
         self.pacman = Pacman()
-        print("Escolha a dificuldade do jogo \n 1- Para Fácil \n 2- Para Médio \n 3- Para Difícil")
-        self.map = Maze(int(input())-1)
+        self.map = Maze()
         self.game_over = None
         self.score = None
         self.time = None
@@ -19,22 +20,25 @@ class Game:
     def init(self):
         os.environ['SDL_VIDEO_CENTERED'] = '1'
         pygame.init()
-        pygame.display.set_caption("SIMPLE PAC MAN INVERTED")
+        pygame.display.set_caption("PACMAN IA")
         self.score = pygame.font.Font(None, 20)
         self.time = pygame.font.Font(None, 20)
-        self.context = pygame.display.set_mode((HEIGHT, WIDTH))
+        self.context = pygame.display.set_mode((WIDTH, HEIGHT))
         self.blocks = pygame.image.load("images/block_maze.png").convert()
         self.blocks = pygame.transform.scale(self.blocks, (45, 45))
         self.frame = pygame.time.Clock()
         self.exit_game = False
-        self.game_over = pygame.font.Font(None, 80).render("GAME OVER", 0, RED)
+        self.game_over = pygame.font.Font(
+            None, 60).render("VOCÊ PERDEU", 0, RED)
 
     def create(self, time):
         self.context.fill(BLACK)
         self.map.create(self.context, self.blocks, 44)
-        self.pacman.create(self.context, YELLOW)
-        self.context.blit(self.score.render(f'Score:{self.pacman.score}', 0, WHITE), (HEIGHT-80, 10))
-        self.context.blit(self.time.render(f'Time:{time}', 0, WHITE), (HEIGHT-80, 30))
+        self.pacman.create(self.context)
+        self.context.blit(self.score.render(
+            f'Pontuação: {self.pacman.score}', 0, WHITE), (WIDTH-250, 10))
+        self.context.blit(self.time.render(
+            f'Tempo Restante: {time}', 0, WHITE), (WIDTH-250, 30))
         self.frame.tick(15)
         pygame.display.update()
 
@@ -58,9 +62,7 @@ class Game:
 
     def execute(self):
         self.init()
-        total_time = 25
-        pygame.mixer.music.load('sounds/game.ogg')
-        pygame.mixer.music.play(2, 8)
+        total_time = 60
 
         while not self.exit_game:
             time = pygame.time.get_ticks()//1000
@@ -71,7 +73,7 @@ class Game:
 
             if total_time - time == 0:
                 self.context.fill(WHITE)
-                self.context.blit(self.game_over, (HEIGHT/2-150, WIDTH/2-30))
+                self.context.blit(self.game_over, (WIDTH/2-150, HEIGHT/2-30))
                 pygame.display.update()
                 pygame.time.delay(3000)
                 self.exit_game = True
@@ -79,12 +81,10 @@ class Game:
                 self.create(total_time-time)
 
             if self.pacman.winner:
-                    win = pygame.font.Font(None, 80).render("YOU WIN", 0, GREEN)
-                    self.context.fill(WHITE)
-                    self.context.blit(win, (HEIGHT/2-150, WIDTH/2-30))
-                    pygame.display.update()
-                    pygame.time.delay(5000)
-                    self.exit_game = True
-
-
-
+                win = pygame.font.Font(None, 60).render(
+                    "VOCÊ GANHOU", 0, GREEN)
+                self.context.fill(WHITE)
+                self.context.blit(win, (WIDTH/2-150, HEIGHT/2-30))
+                pygame.display.update()
+                pygame.time.delay(5000)
+                self.exit_game = True
--- pacman.py
+++ pacman.py
@@ -1,15 +1,12 @@
 import pygame
 
-level = 0
 score = 18
 coins = []
 
 
 class Pacman:
-    global level
     global coins
     x_pos, y_pos, = 1, 1
-    player = None
 
     def __init__(self):
         self.map = Maze()
@@ -17,92 +14,85 @@ class Pacman:
         self.winner = False
         pygame.mixer.init()
         self.song = pygame.mixer.Sound('sounds/eat.wav')
+        self.player = pygame.image.load('images/pacman-right.png')
 
     def move_up(self):
+        self.rotate('up')
         if not self.collid(self.y_pos-1, self.x_pos):
             self.y_pos -= 1
             self.get_coin(self.y_pos, self.x_pos)
 
     def move_down(self):
+        self.rotate('down')
         if not self.collid(self.y_pos+1, self.x_pos):
             self.y_pos += 1
             self.get_coin(self.y_pos, self.x_pos)
 
     def move_left(self):
+        self.rotate('left')
         if not self.collid(self.y_pos, self.x_pos-1):
             self.x_pos -= 1
             self.get_coin(self.y_pos, self.x_pos)
 
     def move_right(self):
+        self.rotate('right')
         if not self.collid(self.y_pos, self.x_pos+1):
             self.x_pos += 1
             self.get_coin(self.y_pos, self.x_pos)
 
-    def create(self, surface, color):
-        #self.player = (pygame.draw.ellipse(surface, color, (self.x_pos*44, self.y_pos*44, 30, 30)))
-        self.player = pygame.image.load('images/ghost.png')
+    def rotate(self, direction):
+        self.player = pygame.image.load(
+            'images/pacman-{}.png'.format(direction))
+
+    def create(self, surface):
+        self.checkWin()
         surface.blit(self.player, (self.x_pos*44, self.y_pos*44))
 
-    def collid(self, i, j):
-        return self.map.levels[level][i][j] == 1
-
-    def get_coin(self, i, j):
-
-        if self.map.levels[level][i][j] == 0:
-            self.score += 100
-            self.song.set_volume(0.05)
-            self.song.play()
-            self.map.levels[level][i][j] = None
+    def checkWin(self):
         if len(coins) == 0:
             self.winner = True
 
+    def collid(self, i, j):
+        return self.map.matriz[i][j] == 1
+
+    def get_coin(self, i, j):
+        if self.map.matriz[i][j] == 0:
+            self.score += 100
+            self.song.set_volume(0.05)
+            self.song.play()
+            self.map.matriz[i][j] = None
+
 
 class Maze:
-    matriz = [[1,1,1,1,1,1,1,1,1,1],
-             [1,0,0,0,0,0,0,0,0,1],
-             [1,0,0,0,1,1,0,0,0,1],
-             [1,0,1,1,1,1,0,0,0,1],
-             [1,0,1,0,0,0,0,0,0,1],
-             [1,0,1,1,1,1,1,0,0,1],
-             [1,0,0,0,0,0,0,0,0,1],
-             [1,1,1,1,1,1,1,1,1,1]]
-
-    matriz2 = [[1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,0,1,0,0,0,0,0,0,0,0,1,1],
-                [1,0,1,0,1,1,0,1,0,0,0,0,1],
-                [1,0,1,1,1,1,0,0,0,1,0,0,1],
-                [1,0,1,0,0,0,0,0,0,1,0,0,1],
-                [1,0,1,1,1,1,1,0,0,1,0,0,1],
-                [1,0,0,0,0,0,0,0,0,1,0,0,1],
-                [1,0,0,1,1,1,0,0,0,0,0,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1]]
-
-    matriz3 = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-                [1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,1],
-                [1,0,1,0,1,1,0,1,0,0,0,0,0,0,0,1],
-                [1,0,1,1,1,1,0,0,0,1,0,0,1,1,1,1],
-                [1,0,1,0,0,0,0,0,0,1,0,0,1,0,1,1],
-                [1,0,1,1,1,1,1,0,0,1,0,0,0,0,0,1],
-                [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1],
-                [1,0,0,1,1,1,0,1,0,0,0,0,0,0,1,1],
-                [1,0,0,1,1,1,0,0,0,0,0,1,1,0,1,1],
-                [1,0,0,1,1,1,0,0,0,0,0,1,1,0,1,1],
-                [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
-
-    levels = (matriz, matriz2, matriz3)
-
-    def __init__(self, nvl=1):
-        self.level = nvl
+    matriz = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+              [1, None, None, None, None, 0, None, None,
+                  None, None, None, 1, None, None, None, 1],
+              [1, 1, 1, 1, 1, 1, None, 1, None, None,
+                  0, None, None, 0, None, 1],
+              [1, 0, None, 1, 1, 1, None, None,
+                  None, 1, None, None, 1, 1, 1, 1],
+              [1, None, None, 1, None, None, None, 0,
+                  None, 1, None, None, 1, 1, 1, 1],
+              [1, None, None, 1, 1, 1, 1, None, None,
+                  1, None, None, None, None, None, 1],
+              [1, 1, None, None, None, None, None, None,
+                  None, 1, None, None, None, None, 0, 1],
+              [1, None, 0, None, 1, 1, None, 1, None,
+                  None, None, None, None, None, 1, 1],
+              [1, None, None, None, 1, 1, 0, None,
+                  None, None, None, 1, 1, None, 1, 1],
+              [1, None, None, None, 1, 1, None, None,
+                  None, None, 0, 1, 1, 0, 1, 1],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
     def create(self, surface, block_maze, size):
-        global level
         global coins
-        level = self.level
         aux_coins = []
-        for i in range(len(self.levels[level])):
-            for j in range(len(self.levels[level][1])):
-                surface.blit(block_maze, (j*size, i*size)) if self.levels[level][i][j] == 1 else \
-                      aux_coins.append(surface.blit(pygame.image.load('images/coin.png'), (j*size, i*size))) \
-                      if self.levels[level][i][j] == 0 else None
+
+        for i in range(len(self.matriz)):
+            for j in range(len(self.matriz[1])):
+                surface.blit(block_maze, (j*size, i*size)) if self.matriz[i][j] == 1 else \
+                    aux_coins.append(surface.blit(pygame.image.load('images/fruit.png'), (j*size, i*size))) \
+                    if self.matriz[i][j] == 0 else None
 
         coins = aux_coins
